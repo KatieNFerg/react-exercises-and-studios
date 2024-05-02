04f9ea8b617be7d2fe1255423148530930fcf0af
KatieNFerg <ketchersidekatie@gmail.com>
2024-05-02 10:06:13 -0500

    react in progress

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I Recommend";
+   let book1 = "https://www.booklistqueen.com/wp-content/uploads/first-lie-wins-ashley-elston.jpg";
+   let book2 = "https://www.booklistqueen.com/wp-content/uploads/the-storm-we-made-vanessa-chan.jpg";
+   let book3 = "https://www.booklistqueen.com/wp-content/uploads/mercury-amy-jo-burns.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="First Lie Wins" />
+         <img src={book2} alt="The Storm We Made" />
+         <img src={book3} alt="Mercury" />
       </div>      
    );
 }
